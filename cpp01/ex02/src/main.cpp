a9c76acd98a0e2c012596a285884316861b0335f
lsouza-r <l182300@dac.unicamp.br>
2025-04-16 22:27:02 -0300

    iniciando ex02

--- cpp01/ex02/src/main.cpp
+++ cpp01/ex02/src/main.cpp
@@ -6,20 +6,20 @@
 /*   By: lsouza-r <lsouza-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 21:05:19 by lsouza-r          #+#    #+#             */
-/*   Updated: 2025/04/16 22:05:42 by lsouza-r         ###   ########.fr       */
+/*   Updated: 2025/04/16 22:20:06 by lsouza-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include "iostream"
+#include "string"
 
 int	main(void) {
-	int	N = 9;
-	Zombie *firstHorde = zombieHorde(N, "zumbi");
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
 
-	if (firstHorde) { 
-		for (int i = 0; i < N; i++)
-			firstHorde[i].announce();
-	}
-	delete[] firstHorde;
-	return 0;
+	std::cout << "Endereço string: " << &str << std::endl;
+	std::cout << "Endereço PTR: " << stringPTR << std::endl;
+	std::cout << "Endereço REF: " << &stringREF << std::endl;
+	
 }
