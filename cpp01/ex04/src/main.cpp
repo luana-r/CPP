5b4c9b5b7c4d34764cce96d89571b10c729ef95e
luana-r <155103553+luana-r@users.noreply.github.com>
2025-04-24 00:09:59 +0000

    ex04 ok

---
+++ cpp01/ex04/src/main.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsouza-r <lsouza-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 21:05:19 by lsouza-r          #+#    #+#             */
+/*   Updated: 2025/04/22 22:20:06 by lsouza-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string replace(std::string line, std::string s1, std::string s2) {
+    std::string result = line;
+    size_t pos = 0;
+    while ((pos = result.find(s1, pos)) != std::string::npos) {
+        result.erase(pos, s1.length());
+        result.insert(pos, s2);
+        pos += s2.length();
+    }
+    return result;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cout << "Try ./replace <filename> <string1> <string2>" << std::endl;
+        return (1);
+    }
+    else {
+        std::string s1 = argv[2];
+        std::string s2 = argv[3];
+        if (s1.empty()) {
+            std::cout << "Error: s1 is empty string" << std::endl;
+            return (1);
+        }
+        std::string filename = argv[1];
+
+        std::ifstream file(filename.c_str());
+        if( !file.is_open() ){
+          std::cout << "Error: failed to open file" << std::endl;
+          return (1);
+        }
+
+        std::string outFileName = filename + "-replace";
+
+        std::ofstream outfile(outFileName.c_str());
+        if ( !outfile.is_open() ) {
+            std::cout << "Error: failed to open output file" << std::endl;
+            file.close();
+            return (1);
+        }
+
+        std::string line;
+        while (std::getline(file, line)) 
+            outfile << replace(line, s1, s2) << std::endl;
+        file.close();
+        outfile.close();
+    }
+    return (0);
+}
